bb73d5cf7957a2432706d070b13db34e865b578f
Biswa Prakash Mishra <biswaprakash444@gmail.com>
2023-05-12 19:23:43 +0530

    infinite loop added

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,34 @@ fn main() {
 
     let mut present_files: HashSet<PathBuf> = HashSet::new();
     let mut new_files: HashSet<PathBuf> = HashSet::new();
-    // Recursively traverse directories
-    traverse_directory(&path,&mut present_files);
-    thread::sleep(Duration::from_secs(15));
-    traverse_directory(&path,&mut new_files);
 
-    let diff1: HashSet<&PathBuf> = present_files.difference(&new_files).collect();
-    let diff2: HashSet<&PathBuf> = new_files.difference(&present_files).collect();
+    loop {
+        traverse_directory(&path,&mut present_files);
+        // Every 15 sec go through the directory to see if any changes
+        thread::sleep(Duration::from_secs(15));
+        traverse_directory(&path,&mut new_files);
 
-    for file2 in diff2 {
-        println!("++{}", file2.display());
-    }
-    for file1 in diff1 {
-        println!("--{}", file1.display());
+        let diff1: HashSet<&PathBuf> = present_files.difference(&new_files).collect();
+        let diff2: HashSet<&PathBuf> = new_files.difference(&present_files).collect();
+
+        for file2 in diff2 {
+            println!("++{}", file2.display());
+        }
+        for file1 in diff1 {
+            println!("--{}", file1.display());
+        }
+
+        //clear the present_files
+        present_files.clear();
+
+        // move the values from new_files to present_files
+        present_files.extend(new_files.drain());
+
+        //clear the new_files
+        new_files.clear();
     }
 }
+
 
 fn traverse_directory(path: &std::path::Path, files: &mut HashSet<PathBuf>){
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
