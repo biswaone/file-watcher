660ff0743250de44e6790c7d6c947fe27c8628fd
Biswa Prakash Mishra <biswaprakash444@gmail.com>
2023-05-12 20:20:13 +0530

    refactor: traverse directory returns files Hashset

--- src/main.rs
+++ src/main.rs
@@ -15,43 +15,28 @@ fn main() {
         Path::new(".")
     };
 
-    let mut present_files: HashSet<PathBuf> = HashSet::new();
-    let mut new_files: HashSet<PathBuf> = HashSet::new();
-
     loop {
-        traverse_directory(&path,&mut present_files);
+        let present_files = traverse_directory(&path);
         // Every 15 sec go through the directory to see if any changes
         thread::sleep(Duration::from_secs(15));
-        traverse_directory(&path,&mut new_files);
+        let new_files = traverse_directory(&path);
 
-        let diff1: HashSet<&PathBuf> = present_files.difference(&new_files).collect();
-        let diff2: HashSet<&PathBuf> = new_files.difference(&present_files).collect();
+        let diff: HashSet<&PathBuf> = present_files.symmetric_difference(&new_files).collect();
 
-        for file2 in diff2 {
-            println!("++{}", file2.display());
+        for file in diff {
+            println!("{}", file.display());
         }
-        for file1 in diff1 {
-            println!("--{}", file1.display());
-        }
-
-        //clear the present_files
-        present_files.clear();
-
-        // move the values from new_files to present_files
-        present_files.extend(new_files.drain());
-
-        //clear the new_files
-        new_files.clear();
     }
 }
 
-
-fn traverse_directory(path: &std::path::Path, files: &mut HashSet<PathBuf>){
+fn traverse_directory(path: &std::path::Path) -> HashSet<PathBuf>{
+    let mut files = HashSet::new();
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
             files.insert(entry.path().to_path_buf());
         }
     }
+    files
 }
 
 
